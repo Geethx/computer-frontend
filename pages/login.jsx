2c4a3d7af17a1cf3f3c68129fc6899857ad3c060
Geethx <jeewangeethanga@gmail.com>
2025-12-20 08:59:40 +0530

    login form created

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
   return (
-    <div>
-    
-    LoginPage
+    <div className="bg-[url('/background.jpg')] bg-cover bg-no-repeat bg-center flex justify-center items-center text-primary w-full h-full">
+        
+        <div className="w-[50%] h-full flex flex-col justify-center items-center">
+          <img src="/logo.png" alt="logo" className='w-[300px]'/>
+          <h1 className='text-4xl text-secondary font-bold mt-5'>Build Your Future!</h1>
+        </div>
+
+        <div className="w-[50%] h-full flex justify-center items-center">
+          <div className="w-[450px] h-[600px] backdrop-blur-3xl shadow-2xl rounded-lg flex flex-col justify-center items-center">
+
+            <input type="email" placeholder="Email" className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-secondary outline-none"/>
+            <input type="password" placeholder="Password" className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-secondary outline-none"/>
+            <p className='w-full text-right pr-5'>Forgot Password <Link to="/forgot-password" className="text-accent">Reset</Link></p>
+
+            
+            <button className="m-5 p-3 w-[90%] h-[50px] bg-accent rounded-lg text-white font-bold">Login</button>
+            <button className="m-5 p-3 w-[90%] h-[50px] border border-accent rounded-lg text-white font-bold">Login with Google</button>
+            <p className='w-full text-right pr-5'>Don't have an account? <Link to="/register" className="text-accent">Register</Link></p>
+
+          </div>
+        </div>
     
     </div>
   )
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,25 +1,25 @@
+
 export default function Test(){
-    <div className="w-[600px] h-[600px] border-4">
+  return (
+    <div className="w-full h-full relative flex flex-wrap">
 
-      <div className="w-[500px] h-[500px] bg-amber-200 flex flex-col justify-center items-center">
-
-        <div className="w-[50px] h-[50px] bg-blue-600">
-        </div>
-
-       <div className="w-[50px] h-[50px] bg-cyan-600 absolute top-50 left-20">
-       </div>
-        
-        <div className="w-[50px] h-[50px] bg-fuchsia-400 fixed left-20">
-        </div>
-
-        <div className="w-[50px] h-[50px] bg-green-500">
-        </div>
-
-        <div className="w-[50px] h-[50px] bg-violet-800">
-        </div>
-
-      </div>  
+      <div className="w-[200px] h-[200px] bg-blue-400 m-[20px] p-[10px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</div>
+      <div className="w-[200px] h-[200px] bg-red-400 m-[20px]">Box2</div>
+      <div className="w-[200px] h-[200px] bg-yellow-400">Box3</div>
+      <div className="w-[200px] h-[200px] bg-green-400">Box4</div>
+      <div className="w-[200px] h-[200px] bg-black">Box5</div>
+      <div className="w-[200px] h-[200px] bg-cyan-400">Box1</div>
+      <div className="w-[200px] h-[200px] bg-purple-400">Box1</div>
+      <div className="w-[200px] h-[200px] bg-pink-400">Box1</div>
+      <div className="w-[200px] h-[200px] bg-amber-400">Box1</div>
+      <div className="w-[200px] h-[200px] bg-emerald-400">Box1</div>
+      <div className="w-[200px] h-[200px] bg-purple-600">Box1</div>
+      <div className="w-[200px] h-[200px] bg-lime-500">Box1</div>
+      <div className="w-[200px] h-[200px] bg-gray-600">Box1</div>
+      <div className="w-[200px] h-[200px] bg-orange-500">Box1</div>
+      <div className="w-[200px] h-[200px] bg-teal-700">Box1</div>
 
     </div>
+  )
 }
 
